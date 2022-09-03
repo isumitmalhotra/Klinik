c5cfc2c29cf0550cefb2477a6efa37aa65328dbb
Savneet Kaur <83158593+savneetkaur03@users.noreply.github.com>
2022-09-03 19:50:23 +0530

    Dashboard Activity

---
+++ app/src/main/java/Dashboard/Doctor.java
@@ -0,0 +1,16 @@
+package Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.excelsior.klinik.R;
+
+public class Doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor);
+    }
+}
