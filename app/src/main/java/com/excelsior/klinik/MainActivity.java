1bdd9f98f7c422ba0bf00c4c7f97940963060dbd
shivchauhan795 <shivchauhan795@gmail.com>
2022-09-04 07:56:22 +0530

    Login/ Register Corrected And Added

--- app/src/main/java/com/excelsior/klinik/MainActivity.java
+++ app/src/main/java/com/excelsior/klinik/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     CardView patient,doctor,chemist;
-
+FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +41,13 @@ public class MainActivity extends AppCompatActivity {
         chemist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(Dashboard.this, Login.class));
+                finish();
+//                break;
+//                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+//                startActivity(intent);
             }
         });
     }
